a23f646eb447ba439c1d1c7a46c8a578e1299bc9
gandb <gandnegro@gmail.com>
2025-01-21 17:59:37 +0000

    Fixing thumbnails

--- scripts/HellowWorld.js
+++ scripts/HellowWorld.js
@@ -1,12 +1,9 @@
-
-
-//este rodou ao instalar
-Hooks.on("ready",   () => {
-  console.log("Hello world2"); 
-	  
- });
- 
 let thumbMapNames =  null;
+
+Hooks.on("ready",   () => {
+ //start game
+});
+ 
  
 //gerando o mapa de scenes
 (async () => {
@@ -15,11 +12,15 @@ let thumbMapNames =  null;
   ["Laboratório", 'laboratorio.webp'],
   ['Main', 'main.webp'],
   ["O Beco Curto", 'beco-curto.webp'],
-  ["White Capel 1888", 'white-capel.webp'],
+  ["White Capel 1888", 'white-capel-1888.webp'],
+  ["White Capel 2024", 'white-capel-2024.webp'],
+  ["Laboratório 2024", 'laboratorio-2024.webp'], 
+  ["Windrush Square", 'windrush-square.webp'],
+  ["Laboratório 1877", 'laboratorio-1877.webp'],
+  ["Laboratório After 2024", 'laboratorio-after-2024.webp'], 
+  ["O Laboratório Antes de 2024", 'laboratorio-before-2024.webp']
 ]);
 })();
-
-
 
 
 Hooks.on("importAdventure",  (adventure, formData, created, updated) => 
@@ -29,17 +30,17 @@ Hooks.on("importAdventure",  (adventure, formData, created, updated) =>
 	{
 		 
 		try{ 
-			console.log(`Start regenerated thumbnail for ${scene.name}`);
+			
 			if(!thumbMapNames.has(scene.name)){
 				return;
 			}
+			console.log(`Start regenerated thumbnail for ${scene.name}`);
 			await scene.update({thumb: "modules/after-time-fate/assets/scenes/"+thumbMapNames.get(scene.name)});
-			console.log("Thumbnail regenrated with success!");
-			
 		}
 		catch(e){
-			console.log(e);
+			console.error(e);
 		}
 	});
+	console.log("Thumbnails regenrated with success!");
  
  });
